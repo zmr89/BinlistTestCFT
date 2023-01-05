5f8c03d3fcaf3144bf888b9e1176a8898dced58c
zmr89 <marsell.ru@gmail.com>
2023-01-05 14:07:22 +0500

    creating HistoryAdapter

--- app/src/main/java/com/example/binlisttestcft/DetailActivity.kt
+++ app/src/main/java/com/example/binlisttestcft/DetailActivity.kt
@@ -31,19 +31,19 @@ class DetailActivity : AppCompatActivity() {
         val binResponse = intent.extras?.get(BINRESPONSE) as BinResponse
         val binNumber = intent.extras?.getString(BIN_NUMBER) as String
 
-        var textViewBINDetail: TextView = findViewById(R.id.textViewBINDetail)
-        var textViewSchemeDetail: TextView = findViewById(R.id.textViewSchemeDetail)
-        var textViewBrandDetail: TextView = findViewById(R.id.textViewBrandDetail)
-        var textViewLengthDetail: TextView = findViewById(R.id.textViewLengthDetail)
-        var textViewLuhnDetail: TextView = findViewById(R.id.textViewLuhnDetail)
-        var textViewTypeDetail: TextView = findViewById(R.id.textViewTypeDetail)
-        var textViewPrepaidDetail: TextView = findViewById(R.id.textViewPrepaidDetail)
-        var textViewCountryNameDetail: TextView = findViewById(R.id.textViewCountryNameDetail)
-        var textViewCountryCodeDetail: TextView = findViewById(R.id.textViewCountryCodeDetail)
-        var textViewCountryCoordinatesDetail: TextView = findViewById(R.id.textViewCountryCoordinatesDetail)
-        var textViewBankNameDetail: TextView = findViewById(R.id.textViewBankNameDetail)
-        var textViewBankWebsiteDetail: TextView = findViewById(R.id.textViewBankWebsiteDetail)
-        var textViewBankPhoneDetail: TextView = findViewById(R.id.textViewBankPhoneDetail)
+        val textViewBINDetail: TextView = findViewById(R.id.textViewBINDetail)
+        val textViewSchemeDetail: TextView = findViewById(R.id.textViewSchemeDetail)
+        val textViewBrandDetail: TextView = findViewById(R.id.textViewBrandDetail)
+        val textViewLengthDetail: TextView = findViewById(R.id.textViewLengthDetail)
+        val textViewLuhnDetail: TextView = findViewById(R.id.textViewLuhnDetail)
+        val textViewTypeDetail: TextView = findViewById(R.id.textViewTypeDetail)
+        val textViewPrepaidDetail: TextView = findViewById(R.id.textViewPrepaidDetail)
+        val textViewCountryNameDetail: TextView = findViewById(R.id.textViewCountryNameDetail)
+        val textViewCountryCodeDetail: TextView = findViewById(R.id.textViewCountryCodeDetail)
+        val textViewCountryCoordinatesDetail: TextView = findViewById(R.id.textViewCountryCoordinatesDetail)
+        val textViewBankNameDetail: TextView = findViewById(R.id.textViewBankNameDetail)
+        val textViewBankWebsiteDetail: TextView = findViewById(R.id.textViewBankWebsiteDetail)
+        val textViewBankPhoneDetail: TextView = findViewById(R.id.textViewBankPhoneDetail)
 
         textViewBINDetail.text = "BIN: $binNumber"
         textViewSchemeDetail.text = "SCHEME/NETWORK: " + binResponse.scheme
@@ -54,18 +54,14 @@ class DetailActivity : AppCompatActivity() {
         textViewPrepaidDetail.text = "PREPAID:  ${if (binResponse.prepaid == true) {" Yes "} else {" No "}}"
         textViewCountryNameDetail.text = "COUNTRY NAME: " + binResponse.country?.countyName
         textViewCountryCodeDetail.text = "COUNTRY CODE: " + binResponse.country?.countryCode
-        textViewCountryCoordinatesDetail.text ="COUNTRY COORDINATES: " +
+        textViewCountryCoordinatesDetail.text = "COUNTRY COORDINATES: " +
             "latitude ${binResponse.country?.latitude}, longitude ${binResponse.country?.longitude}"
         textViewBankNameDetail.text = "BANK NAME: " + binResponse.bank?.bankName
         textViewBankWebsiteDetail.text = "BANK WEBSITE: " + binResponse.bank?.url
         textViewBankPhoneDetail.text = "BANK PHONE: " + binResponse.bank?.phone
 
         val detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-//        detailViewModel.isSuccessInsertLD.observe(this, Observer {
-//
-//        })
         detailViewModel.insertBinDB(Bin(binNumber = binNumber, binResponse = binResponse))
-
     }
 
 
